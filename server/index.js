eccc6c41cce156c77dca54c3825b910731c19d73
Darshan Dinesh <darshandineshmp@gmail.com>
2024-08-27 00:32:11 +0530

    server port changed

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { login, signup } from "./controllers/auth.controller.js";
 import { createTransaction, deleteTransaction, editTransaction, getTransactions } from "./controllers/transaction.controller.js";
 import cors from 'cors';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
